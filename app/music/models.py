5ba3a0132c3386bd669cbca032f1143f23176488
Sahil H. Mobaidin <Sahil.hmob@hotmail.com>
2019-04-12 13:51:23 +0300

    add max_length to models

--- app/music/models.py
+++ app/music/models.py
@@ -2,14 +2,14 @@ from django.db import models
 
 
 class Artist(models.Model):
-    name = models.CharField()
+    name = models.CharField(max_length=50)
 
 
 class Album(models.Model):
-    name = models.CharField()
+    name = models.CharField(max_length=50)
     artist = models.ForeignKey(Artist, on_delete=models.CASCADE)
 
 
 class Song(models.Model):
-    name = models.CharField()
+    name = models.CharField(max_length=50)
     album = models.ForeignKey(Album, on_delete=models.CASCADE)
