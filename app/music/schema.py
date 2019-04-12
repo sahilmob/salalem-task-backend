c04fea7814a39ac2fb72d83cd945146a2f3fe33e
Sahil H. Mobaidin <Sahil.hmob@hotmail.com>
2019-04-12 15:43:57 +0300

    add artist all albums resolver

--- app/music/schema.py
+++ app/music/schema.py
@@ -1,7 +1,7 @@
 import graphene
 from graphene_django import DjangoObjectType
 
-from .models import Artist
+from .models import Artist, Album
 
 
 class ArtistType(DjangoObjectType):
@@ -9,8 +9,18 @@ class ArtistType(DjangoObjectType):
         model = Artist
 
 
+class AlbumType(DjangoObjectType):
+    class Meta:
+        model = Album
+
+
 class Query(object):
     all_artists = graphene.List(ArtistType)
+    artist_all_albums = graphene.List(AlbumType, artist=graphene.Int())
 
     def resolve_all_artists(self, info, **kwargs):
         return Artist.objects.all()
+
+    def resolve_artist_all_albums(self, info, **kwargs):
+        artist = kwargs.get('artist')
+        return Album.objects.filter(artist__exact=artist)
