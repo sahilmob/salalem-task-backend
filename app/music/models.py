186172d483e2fae19e9ea3473c56247ad066d0b5
Sahil H. Mobaidin <Sahil.hmob@hotmail.com>
2019-04-12 13:47:54 +0300

    create song model

--- app/music/models.py
+++ app/music/models.py
@@ -8,3 +8,8 @@ class Artist(models.Model):
 class Album(models.Model):
     name = models.CharField()
     artist = models.ForeignKey(Artist, on_delete=models.CASCADE)
+
+
+class Song(models.Model):
+    name = models.CharField()
+    album = models.ForeignKey(Album, on_delete=models.CASCADE)
