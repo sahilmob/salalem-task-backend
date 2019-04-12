5f133c634619f1d7a152a3708a8e761071579fcb
Sahil H. Mobaidin <Sahil.hmob@hotmail.com>
2019-04-12 14:09:50 +0300

    create all_artists resolver

--- app/music/schema.py
+++ app/music/schema.py
@@ -7,3 +7,10 @@ from .models import Artist
 class ArtistType(DjangoObjectType):
     class Meta:
         model = Artist
+
+
+class Query(object):
+    all_artists = graphene.List(ArtistType)
+
+    def resolve_all_artists(self, info, **kwargs):
+        return Artist.objects.all()
