8455b2b2a3e190d4e32de2b9097326a8b43dc4ba
Sahil H. Mobaidin <Sahil.hmob@hotmail.com>
2019-04-12 13:46:07 +0300

    create album model

--- app/music/models.py
+++ app/music/models.py
@@ -3,3 +3,8 @@ from django.db import models
 
 class Artist(models.Model):
     name = models.CharField()
+
+
+class Album(models.Model):
+    name = models.CharField()
+    artist = models.ForeignKey(Artist, on_delete=models.CASCADE)
